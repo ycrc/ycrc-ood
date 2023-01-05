e8b89bf06b18ae46b68b3c3abac68386aa5e55a5
luop0812@gmail.com <luop0812@gmail.com>
2023-01-05 18:34:39 -0500

    fix the bug which returns incomplete partitions allowed for a given user

--- lib/partitions.sh
+++ lib/partitions.sh
@@ -2,5 +2,5 @@
 
 username=$1
 # get the partition name and TRES value of the private partitions for a specific user
-cmd="for ACCOUNT in $(sacctmgr show user name=$username -s -n -p | cut -d\| -f2); do scontrol show partition -o | egrep \$ACCOUNT | cut -d' ' -f1,33; done;"
+cmd="for ACCOUNT in $(sacctmgr show user name=$username -s -n -p | cut -d\| -f5); do scontrol show partition -o | egrep \$ACCOUNT | cut -d' ' -f1,33; done;"
 eval $cmd
